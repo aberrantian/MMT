bc7f80d36e35283dcba62696f1f26e4736fe1778
aberrantian <aberrantian@gmail.cm>
2023-02-19 19:29:19 +0000

    setup variables

--- scripts/init.js
+++ scripts/init.js
@@ -2,3 +2,25 @@
 This script will collect mode options into a json file
 for use in interactive mode.
 */
+
+// input type checkbox
+const allBox = document.getElementById('all');
+const addBox = document.getElementById('addition');
+const subBox = document.getElementById('subtraction');
+const multBox = document.getElementById('multiplication');
+const divBox = document.getElementById('division');
+
+const decimalBox = document.getElementById('decimals');
+const recurBox = document.getElementById('recurring');
+const negBox = document.getElementById('negatives');
+
+// input type number
+const digits = document.getElementById('digits');
+
+
+const allBoxTargets = [
+    addBox,
+    subBox,
+    multBox,
+    divBox
+]
