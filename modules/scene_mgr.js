1a5e1dfcee962cc491f758e38c6cd9e16e0bf066
aberrantian <aberrantian@gmail.com>
2023-02-25 21:31:41 +0000

    rewrite scene_mgr.js

--- modules/scene_mgr.js
+++ modules/scene_mgr.js
@@ -1,8 +1,15 @@
 import { mainMenu } from "./scenes/main_menu.js"
+import { about } from "./scenes/about.js";
+
+export function change (scene) {
+    activeScene.hidden = true;
+    activeScene = scene;
+    activeScene.hidden = false;
+};
 
 export const scene = {
-    change: function () {
-
-    },
     mainMenu: mainMenu,
+    about: about,
 };
+
+export let activeScene = scene.mainMenu;
