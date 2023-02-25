2d79e92113902f9f5576a07ad8d1fd1b29be3c14
aberrantian <aberrantian@gmail.com>
2023-02-25 20:01:09 +0000

    change 'let' to 'const' in main_menu.js

--- modules/scenes/main_menu.js
+++ modules/scenes/main_menu.js
@@ -1,32 +1,32 @@
-export let mainMenu = document.createElement('div');
+export const mainMenu = document.createElement('div');
 mainMenu.setAttribute('id', 'main-menu');
 
-let arithmetic = document.createElement('div');
+const arithmetic = document.createElement('div');
 arithmetic.setAttribute('id', 'arithmetic-button');
 arithmetic.setAttribute('class', 'button');
 arithmetic.textContent = 'arithmetic';
 
-let multTables = document.createElement('div');
+const multTables = document.createElement('div');
 multTables.setAttribute('id', 'mult-tables-button');
 multTables.setAttribute('class', 'button');
 multTables.textContent = 'multiplication tables';
 
-let unitCon = document.createElement('div');
+const unitCon = document.createElement('div');
 unitCon.setAttribute('id', 'unit-con-button');
 unitCon.setAttribute('class', 'button');
 unitCon.textContent = 'unit conversion';
 
-let percentages = document.createElement('div');
+const percentages = document.createElement('div');
 percentages.setAttribute('id', 'percentages-button');
 percentages.setAttribute('class', 'button');
 percentages.textContent = 'percentages';
 
-let settings = document.createElement('div');
+const settings = document.createElement('div');
 settings.setAttribute('id', 'settings-button');
 settings.setAttribute('class', 'button');
 settings.textContent = 'settings';
 
-let about = document.createElement('div');
+const about = document.createElement('div');
 about.setAttribute('id', 'about-button');
 about.setAttribute('class', 'button');
 about.textContent = 'about';
