94105f6bdeae0931a4b93ab64f077a9dbea14ed8
aberrantian <aberrantian@gmail.com>
2023-02-25 16:45:18 +0000

    update main menu

--- modules/scenes/main_menu.js
+++ modules/scenes/main_menu.js
@@ -1,7 +1,46 @@
 export let mainMenu = document.createElement('div');
 mainMenu.setAttribute('id', 'main-menu');
 
-let test = document.createElement('p');
-test.textContent = 'hello world';
+let arithmetic = document.createElement('div');
+arithmetic.setAttribute('id', 'arithmetic-button');
+arithmetic.setAttribute('class', 'button');
+arithmetic.textContent = 'arithmetic';
 
-mainMenu.appendChild(test);
+let multTables = document.createElement('div');
+multTables.setAttribute('id', 'mult-tables-button');
+multTables.setAttribute('class', 'button');
+multTables.textContent = 'multiplication tables';
+
+let unitCon = document.createElement('div');
+unitCon.setAttribute('id', 'unit-con-button');
+unitCon.setAttribute('class', 'button');
+unitCon.textContent = 'unit conversion';
+
+let percentages = document.createElement('div');
+percentages.setAttribute('id', 'percentages-button');
+percentages.setAttribute('class', 'button');
+percentages.textContent = 'percentages';
+
+let settings = document.createElement('div');
+settings.setAttribute('id', 'settings-button');
+settings.setAttribute('class', 'button');
+settings.textContent = 'settings';
+
+let about = document.createElement('div');
+about.setAttribute('id', 'about-button');
+about.setAttribute('class', 'button');
+about.textContent = 'about';
+
+const elements = [
+    arithmetic,
+    multTables,
+    unitCon,
+    percentages,
+    settings,
+    about
+]
+
+
+for (const element of elements) {
+    mainMenu.appendChild(element);
+};
