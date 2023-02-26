523ceaa05cd6c75950614e6edc188cb91582b827
aberrantian <aberrantian@gmail.com>
2023-02-26 19:11:57 +0000

    add classList management to change function

--- modules/scene_mgr.js
+++ modules/scene_mgr.js
@@ -3,8 +3,12 @@ import { about } from "./scenes/about.js";
 
 export function change (scene) {
     activeScene.hidden = true;
+    activeScene.classList.remove('active-scene');
+
     activeScene = scene;
+    
     activeScene.hidden = false;
+    activeScene.classList.add('active-scene');
 };
 
 export const scene = {
