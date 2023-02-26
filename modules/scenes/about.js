06ed1dd55492f7c831692178bcf0b5ffcaac814a
aberrantian <aberrantian@gmail.com>
2023-02-26 17:04:19 +0000

    rewrite description and add links

--- modules/scenes/about.js
+++ modules/scenes/about.js
@@ -9,6 +9,22 @@ mainMenuBtn.addEventListener('click', () => {
 });
 
 
-const desc = create.div('div1', 'text', 'description');
+const textA = document.createTextNode(`
+MMT (Mental Math Trainer) focuses primarily on mental math skills.
+Teaching subjects of mathematics is secondary. Here is the
+`);
 
+const sourceLink = create.link(null, 'link', 'source code', 'https://github.com/aberrantian/MMT', true);
+
+const textB = document.createTextNode(`
+for this app and here is the
+`);
+
+const devLink = create.link(null, null, "developer's website", 'https://aberrantian.github.io', true);
+
+const textC = document.createTextNode('.');
+
+const desc = create.p();
+
+create.appends(desc, textA, sourceLink, textB, devLink, textC);
 create.appends(about, mainMenuBtn, desc);
