09f477bf462b93e73ac848e2c6613d2c613b7666
aberrantian <aberrantian@gmail.com>
2023-02-28 16:37:48 +0000

    create pseudocode layout for arithmetic menu

--- modules/scenes/arithmetic.js
+++ modules/scenes/arithmetic.js
@@ -3,6 +3,13 @@ import { create } from "../create.js";
 
 export const arithmetic = create.div('arithmetic');
 
+/* Layout
+scoreboard
+start
+settings
+main menu
+*/
+
 const mainMenuBtn = create.btn('main-menu-button', null, 'main menu');
 mainMenuBtn.addEventListener('click', () => {
     change(scene.mainMenu);
