a2de351b14b40581830fe35c9450890acd69e667
aberrantian <aberrantian@gmail.com>
2023-02-28 18:33:30 +0000

    fix layout

--- modules/scenes/arithmetic.js
+++ modules/scenes/arithmetic.js
@@ -3,8 +3,15 @@ import { create } from "../create.js";
 
 export const arithmetic = create.div('arithmetic');
 
+const header = create.div(null, 'header', null);
 const scoreboard = create.div('scoreboard', null, 'scoreboard');
+header.appendChild(scoreboard);
+
+const main = create.div(null, 'main', null);
+const buttonContainer = create.div(null, 'button-container', null);
 const startBtn = create.btn(null, null, 'start');
 const settingsBtn = create.btn(null, null, 'settings');
+create.appends(buttonContainer, startBtn, settingsBtn);
+create.appends(main, buttonContainer);
 
-create.appends(arithmetic, scoreboard, startBtn, settingsBtn);
+create.appends(arithmetic, header, main);
