ce9ff9d19092cfcc6b789a9ab0c7c688581b5ac9
aberrantian <aberrantian@gmail.com>
2023-02-28 19:47:17 +0000

    rename settings const

--- modules/scenes/settings.js
+++ modules/scenes/settings.js
@@ -1,11 +1,11 @@
 import { change, scene } from "../scene_mgr.js";
 import { create } from "../create.js";
 
-export const settings = create.div('settings');
+export const settingsScene = create.div('settings');
 
 const mainMenuBtn = create.btn(null, null, 'main menu');
 mainMenuBtn.addEventListener('click', () => {
     change(scene.mainMenu);
 });
 
-settings.appendChild(mainMenuBtn);
+settingsScene.appendChild(mainMenuBtn);
