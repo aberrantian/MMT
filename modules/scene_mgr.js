eb64610b8911ebb75e02361285a321a16ea48be4
aberrantian <aberrantian@gmail.com>
2023-02-28 18:08:04 +0000

    scale down project scope

--- modules/scene_mgr.js
+++ modules/scene_mgr.js
@@ -1,10 +1,5 @@
-import { mainMenu } from "./scenes/main_menu.js"
 import { arithmetic } from "./scenes/arithmetic.js";
-import { multTables } from "./scenes/mult_tables.js";
-import { unitCon } from "./scenes/unit_con.js";
-import { percentages } from "./scenes/percentages.js";
 import { settings } from "./scenes/settings.js";
-import { about } from "./scenes/about.js";
 
 export function change (scene) {
     activeScene.hidden = true;
@@ -17,13 +12,8 @@ export function change (scene) {
 };
 
 export const scene = {
-    mainMenu: mainMenu,
     arithmetic: arithmetic,
-    multTables: multTables,
-    unitCon: unitCon,
-    percentages: percentages,
     settings: settings,
-    about: about,
 };
 
-export let activeScene = scene.mainMenu;
+export let activeScene = scene.arithmetic;
--- modules/scenes/arithmetic.js
+++ modules/scenes/arithmetic.js
@@ -3,20 +3,8 @@ import { create } from "../create.js";
 
 export const arithmetic = create.div('arithmetic');
 
-/* Layout
-scoreboard
-start
-settings
-main menu
-*/
-
 const scoreboard = create.div('scoreboard', null, 'scoreboard');
 const startBtn = create.btn(null, null, 'start');
 const settingsBtn = create.btn(null, null, 'settings');
 
-const mainMenuBtn = create.btn('main-menu-button', null, 'main menu');
-mainMenuBtn.addEventListener('click', () => {
-    change(scene.mainMenu);
-});
-
-create.appends(arithmetic, scoreboard, startBtn, settingsBtn, mainMenuBtn);
+create.appends(arithmetic, scoreboard, startBtn, settingsBtn);
